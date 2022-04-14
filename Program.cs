c2639953f1666a8198db1afaa9aa8a0681a6ac30
gregory <wiznergregory@gmail.com>
2022-04-14 19:24:42 -0400

    Exercize5

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+double userIn = 0;
+double userBin = 0;
+
+Console.WriteLine("Choose a number:");
+string userInput = Console.ReadLine();
+double.TryParse(userInput, out userIn);
+Console.WriteLine();
+Console.WriteLine("Choose one more number:");
+string userBinput = Console.ReadLine();
+double.TryParse(userBinput, out userBin);
+Console.WriteLine();
+Console.WriteLine(userIn * userBin);
+Console.ReadLine();
